d821ca61ae48e8ba787b39dda4bf0245591e344b
karen-mtz <dkareen.mtz@gmail.com>
2019-07-19 11:37:47 -0500

    l

--- src/app.js
+++ src/app.js
@@ -3,74 +3,74 @@ let formData = {};
 let formExpense = {};
 
 const onFormSubmit = () => {
-  if (validate()) {
-    let formData = readFormData();
-    if (selectedRow == null) insertNewRecord(formData);
-    else updateRecord(formData);
-    resetForm();
-  }
+ if (validate()) {
+   let formData = readFormData();
+   if (selectedRow == null) insertNewRecord(formData);
+   else updateRecord(formData);
+   resetForm();
+ }
 };
 
 const readFormData = () => {
-  formData["project"] = document.getElementById("project").value;
-  formData["budget"] = document.getElementById("budget").value;
-  console.log(formData);
-  return formData;
+ formData["project"] = document.getElementById("project").value;
+ formData["budget"] = document.getElementById("budget").value;
+ console.log(formData);
+ return formData;
 };
 
 const insertNewRecord = data => {
-  document.getElementById("form").style.visibility = "hidden";
-  let table = document
-    .getElementById("budgetList")
-    .getElementsByTagName("tbody")[0];
-  let newRow = table.insertRow(table.length);
-  cell1 = newRow.insertCell(0);
-  cell1.innerHTML = data.project;
-  row2 = newRow.insertCell(1);
-  row2.innerHTML = `Presupuesto: $${data.budget}`;
-  cell3 = newRow.insertCell(2);
-  cell3.innerHTML = `<i class="fas fa-edit" onClick="onEdit(this)"></i>
-                    <i class="far fa-trash-alt delete"onClick="onDelete(this)"></i>`;
-  document.getElementById("add-expence").innerHTML = `<footer class="footer">
-  
+ document.getElementById("form").style.visibility = "hidden";
+ let table = document
+   .getElementById("budgetList")
+   .getElementsByTagName("tbody")[0];
+ let newRow = table.insertRow(table.length);
+ cell1 = newRow.insertCell(0);
+ cell1.innerHTML = data.project;
+ row2 = newRow.insertCell(1);
+ row2.innerHTML = `Presupuesto: $${data.budget}`;
+ cell3 = newRow.insertCell(2);
+ cell3.innerHTML = `<i class="fas fa-edit" onClick="onEdit(this)"></i>
+ <i class="far fa-trash-alt delete"onClick="onDelete(this)"></i>`;
+ document.getElementById("add-expence").innerHTML = `<footer class="footer">
+ 
 <i class="fas fa-plus-circle" onClick="addexpense(this)"></i> </footer>`;
 };
 
 const resetForm = () => {
-  document.getElementById("project").value = "";
-  document.getElementById("budget").value = "";
-  selectedRow = null;
+ document.getElementById("project").value = "";
+ document.getElementById("budget").value = "";
+ selectedRow = null;
 };
 
 const onEdit = td => {
-  document.getElementById("form").style.visibility = "visible";
-  selectedRow = td.parentElement.parentElement;
-  document.getElementById("project").value = selectedRow.cells[0].innerHTML;
-  document.getElementById("budget").value = selectedRow.cells[1].innerHTML;
+ document.getElementById("form").style.visibility = "visible";
+ selectedRow = td.parentElement.parentElement;
+ document.getElementById("project").value = selectedRow.cells[0].innerHTML;
+ document.getElementById("budget").value = selectedRow.cells[1].innerHTML;
 };
 const updateRecord = formData => {
-  document.getElementById("form").style.visibility = "hidden";
-  selectedRow.cells[0].innerHTML = formData.project;
-  selectedRow.cells[1].innerHTML = formData.budget;
+ document.getElementById("form").style.visibility = "hidden";
+ selectedRow.cells[0].innerHTML = formData.project;
+ selectedRow.cells[1].innerHTML = formData.budget;
 };
 
 const onDelete = td => {
-  if (confirm("Are you sure to delete this record ?")) {
-    row = td.parentElement.parentElement;
-    document.getElementById("budgetList").deleteRow(row.rowIndex);
-    resetForm();
-  }
+ if (confirm("Are you sure to delete this record ?")) {
+   row = td.parentElement.parentElement;
+   document.getElementById("budgetList").deleteRow(row.rowIndex);
+   resetForm();
+ }
 };
 
 const addexpense = () => {
-  const innerTable = document.getElementById("secondtable");
-  innerTable.style.display = "block";
+ const innerTable = document.getElementById("secondtable");
+ innerTable.style.display = "block";
 
-  innerTable.innerHTML = `
- 
- 
- <td>
- <form id="form-expenses" onsubmit="event.preventDefault();onFormSubmit();" autocomplete="off">
+ innerTable.innerHTML = `
+
+
+<td>
+<form id="form-expenses" onsubmit="event.preventDefault();onFormSubmit();" autocomplete="off">
      <div>
          <label></label><label class="validation-error hide" id="projectValidationError"></label>
          <input type="number" name="quantity" id="quantity" placeholder="$0.00" />
@@ -90,49 +90,49 @@ const addexpense = () => {
 };
 
 const createExpense = () => {
-  let formExpense = expenseData();
-  let totalQuantity = formExpense.quantity * formExpense.multiplier;
-  let newbudget = formData.budget - totalQuantity;
-  formData.budget = newbudget;
-  formData.total = totalQuantity;
-  row2.innerHTML = formData.budget;
-  const visibility = document.getElementById("expensesList");
-  const addExpen = document.getElementById("secondtable");
+ let formExpense = expenseData();
+ let totalQuantity = formExpense.quantity * formExpense.multiplier;
+ let newbudget = formData.budget - totalQuantity;
+ formData.budget = newbudget;
+ formData.total = totalQuantity;
+ row2.innerHTML = formData.budget;
+ const visibility = document.getElementById("expensesList");
+ const addExpen = document.getElementById("secondtable");
 
-  visibility.style.display = "block";
-  addExpen.style.display = "none";
+ visibility.style.display = "block";
+ addExpen.style.display = "none";
 
-  console.log();
-  console.log(formData);
-  insertNewExpense(formExpense);
-  resetForm();
+ console.log();
+ console.log(formData);
+ insertNewExpense(formExpense);
+ resetForm();
 };
 
 const expenseData = () => {
-  formExpense["quantity"] = document.getElementById("quantity").value;
-  formExpense["concept"] = document.getElementById("concept").value;
-  formExpense["multiplier"] = document.getElementById("multiplier").value;
-  console.log(formExpense);
+ formExpense["quantity"] = document.getElementById("quantity").value;
+ formExpense["concept"] = document.getElementById("concept").value;
+ formExpense["multiplier"] = document.getElementById("multiplier").value;
+ console.log(formExpense);
 
-  return formExpense;
+ return formExpense;
 };
 
 const onDeleteExpense = td => {
-  if (confirm("Are you sure to delete this record ?")) {
-    row = td.parentElement.parentElement;
-    formData.budget = formData.budget + formData.total;
-    row2.innerHTML = formData.budget;
-    document.getElementById("expensesList").deleteRow(row.rowIndex);
-    resetForm();
-  }
+ if (confirm("Are you sure to delete this record ?")) {
+   row = td.parentElement.parentElement;
+   formData.budget = formData.budget + formData.total;
+   row2.innerHTML = formData.budget;
+   document.getElementById("expensesList").deleteRow(row.rowIndex);
+   resetForm();
+ }
 };
 
 const insertNewExpense = data => {
-  document.getElementById("form").style.visibility = "hidden";
-  let tab = document
-    .getElementById("expensesList")
-    .getElementsByTagName("tbody")[0];
-  let newRow = tab.insertRow(tab.length);
+ document.getElementById("form").style.visibility = "hidden";
+ let tab = document
+   .getElementById("expensesList")
+   .getElementsByTagName("tbody")[0];
+ let newRow = tab.insertRow(tab.length);
   // cell1 = newRow.insertCell(0);
   // // cell1.innerHTML = data.concept;
   // cell2 = newRow.insertCell(1);
@@ -149,18 +149,18 @@ const insertNewExpense = data => {
 };
 
 const validate = () => {
-  isValid = true;
-  if (document.getElementById("project").value == "") {
-    isValid = false;
-    document.getElementById("projectValidationError").classList.remove("hide");
-  } else {
-    isValid = true;
-    if (
-      !document
-        .getElementById("projectValidationError")
-        .classList.contains("hide")
-    )
-      document.getElementById("projectValidationError").classList.add("hide");
-  }
-  return isValid;
+ isValid = true;
+ if (document.getElementById("project").value == "") {
+   isValid = false;
+   document.getElementById("projectValidationError").classList.remove("hide");
+ } else {
+   isValid = true;
+   if (
+     !document
+       .getElementById("projectValidationError")
+       .classList.contains("hide")
+   )
+     document.getElementById("projectValidationError").classList.add("hide");
+ }
+ return isValid;
 };
